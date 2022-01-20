4f78a98e4d7032d0e03dd16ed18003525c73585d
NongNoooo <83653104+NongNoooo@users.noreply.github.com>
2022-01-20 16:38:43 +0900

    Rotation controller complete

--- Shooting/Assets/Scripts/Move.cs
+++ Shooting/Assets/Scripts/Move.cs
@@ -4,99 +4,272 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum GoHorizontal
     {
-        
+        goRight,
+        goLeft,
+        noDir,
     }
 
-    public bool goDown = false;
-    public bool goUp = false;
-    public bool goLeft = false;
-    public bool goRight = false;
+    public enum GoVertical
+    {
+        goDown,
+        goUp,
+        noDir,
+    }
 
+    public GoHorizontal goHorizontal;
+    public GoVertical goVertical;
 
     float mh;
     float mv;
 
-    float xSpeed;
-    float ySpeed;
+    public float xSpeed;
+    public float ySpeed;
 
-    // Update is called once per frame
+    float mOriPosX;
+    float mOriPosY;
+
+    //방향 전환 속도 맥스값
+    float maxTurningSpeed = 10.0f;
+
+    //방향전환을 입력받기 시작할 마우스 위치값
+    float goDirValue = 100.0f;
+
+    void Start()
+    {
+        //게임 시작시 마우스 커서 안보이도록
+        Cursor.lockState = CursorLockMode.Locked;
+        goHorizontal = GoHorizontal.noDir;
+        goVertical = GoVertical.noDir;
+
+        MousePositionInit();
+    }
+
+    void MousePositionInit()
+    {
+        //게임 시작시 마우스 위치값을 받아옴
+        mOriPosX = Input.mousePosition.x;
+        mOriPosY = Input.mousePosition.y;
+    }
+
+    private void FixedUpdate()
+    {
+        //회전하는 코드
+        Turing();
+    }
+
+    Vector3 toRotation;
+
+    void Turing()
+    {
+        Vector3 dir = new Vector3(-ySpeed, xSpeed, 0.0f);
+
+        //90도에서 더 이상 회전안함
+        //오일러 앵글은 음수값으로 더해지지 못함
+        //따라서 아래처럼 대입방식으로 작성해야함
+        //transform.eulerAngles += dir * 10/*임의의값*/ * Time.deltaTime;
+
+        //백터3 변수에 대입한 후에
+        toRotation += 10 * dir * Time.deltaTime;
+
+        TurningLock();
+
+        //각도를 x로 지정하면 정상적으로 회전함
+        transform.eulerAngles = toRotation;
+
+
+
+        //정상적으로 회전함
+        //그런데 너무 즉각적으로 회전함
+        /* transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(dir), 1.0f);
+
+         if (transform.rotation.eulerAngles.x < -120.0f)
+         {
+             transform.eulerAngles = new Vector3(-120.0f, transform.rotation.y, transform.rotation.z);
+         }*/
+    }
+
+    void TurningLock()
+    {
+        //회전 각도 제한
+        toRotation.x = Mathf.Clamp(toRotation.x, -120.0f, 120.0f);
+        toRotation.y = Mathf.Clamp(toRotation.y, -120.0f, 120.0f);
+    }
+
     void Update()
     {
-        //마우스 X축Y축 입력받음
-        /*        mh = Input.GetAxis("Mouse X");
-                mv = Input.GetAxis("Mouse Y");
-        */
+        //마우스 위치 받아오는 메서드
+        MousePosition();
 
-        mh = Input.mousePosition.x;
-        mv = Input.mousePosition.y;
+        //마우스 위치에 따라 어느방향으로 회전할지 결정하는 메서드
+        MouseHorizontalPosCheck();
+        MouseVerticalPosCheck();
 
-        LeftRight();
-        UpDown();
-
-        Hvelocity();
-
-
-        Vector3 dir = new Vector3(0, xSpeed, 0.0f);
-
-
-
-        transform.eulerAngles += 10 * dir * Time.deltaTime;
+        //결정된 회전방향에 따라 회전 작동
+        HorizontalRotation();
+        VeticalRotation();
     }
 
-    void UpDown()
+    void MousePosition()
     {
-        //입력방향에 따라 그에맞는 불값을 TRUE로 변경
-        if (mv < 0)
-        {
-            goUp = false;
-            goDown = true;
-        }
-        else if (mh > 0)
-        {
-            goDown = false;
-            goUp = true;
-        }
-    }
+        //마우스 위치값에 게임시작시에 받아온 마우스 위치값을 빼서
+        //게임시작시 mh, mv가 0으로 시작할수 있게 만듬
+        mh = Input.mousePosition.x - mOriPosX;
+        mv = Input.mousePosition.y - mOriPosY;
 
-    void LeftRight()
+        mousPosLock();
+
+        Debug.Log("x = " + mh + "  y = " + mv);
+    }
+    void mousPosLock()
     {
-        if (mh < 0)
-        {
-            goRight = false;
-            goLeft = true;
-        }
-        else if (mv > 0)
-        {
-            goLeft = false;
-            goRight = true;
-        }
+        //마우스 입력값이 300, -300보다 못커지게 만듬
+
+        mh = Mathf.Clamp(mh, -300.0f, 300.0f);
+        mv = Mathf.Clamp(mv, -300.0f, 300.0f);
     }
 
-    void Hvelocity()
+
+    //마우스 위치에 따라 현재 상태 스위치
+    void MouseHorizontalPosCheck()
     {
-        //현제 방향 불값에 따라 그 방향으로 속도 점점 가속
-        if (goRight)
+        if(mh > -goDirValue && mh < goDirValue)
         {
-            xSpeed += 1 * Time.deltaTime;
+            goHorizontal = GoHorizontal.noDir;
         }
-        if (goLeft)
+        else if (mh > goDirValue)
         {
-            xSpeed -= 1 * Time.deltaTime;
+            //mh가 goDirValu(100)보다 커지면 오른쪽으로 회전
+            goHorizontal = GoHorizontal.goRight;
         }
-
-     /*   if (xSpeed < 0)
+        else if (mh < -goDirValue)
         {
-            if (xSpeed > -10)
-            {
-                xSpeed = 10;
-            }
-        }
-        else if(xSpeed > 0)
-        {
-            if()
-        }*/
+            //mh가 -goDirValu(100)보다 작아지면 왼쪽으로 회전
+            goHorizontal = GoHorizontal.goLeft;
+        }    
     }
+    void MouseVerticalPosCheck()
+    {
+        if (mv > -goDirValue && mv < goDirValue)
+        {
+            goVertical = GoVertical.noDir;
+        }
+        else if (mv > goDirValue)
+        {
+            goVertical = GoVertical.goDown;
+        }
+        else if (mv < -goDirValue)
+        {
+            goVertical = GoVertical.goUp;
+        }
+    }
+
+
+    //상하 회전 스위치 문과 현재 상태에 따른 회전 메서드
+    void VeticalRotation()
+    {
+        switch (goVertical)
+        {
+            case GoVertical.goUp:
+                GoUp();
+                return;
+            case GoVertical.goDown:
+                GoDown();
+                return;
+            case GoVertical.noDir:
+                StopVeticalRotation();
+                return;
+        }
+    }
+    void GoUp()
+    {
+        //회전 방향에 맞게 회전에 가속도 줌
+        ySpeed += 5 * Time.deltaTime;
+
+        SpeedLimit(ref ySpeed);
+    }
+    void GoDown()
+    {
+        //회전 방향에 맞게 회전에 가속도 줌
+        ySpeed -= 5 * Time.deltaTime;
+
+        SpeedLimit(ref ySpeed);
+    }
+    void StopVeticalRotation()
+    {
+        //마우스가 중앙으로 오면 상하 회전 가속을 0으로 만들어
+        //더 이상 회전하지 않게 만듬
+        /*  if(ySpeed < 0)
+          {
+              //ySpeed += 10 * Time.deltaTime;
+              ySpeed = Mathf.Lerp(ySpeed, 0.0f, 1.0f);
+          }
+          else if(ySpeed > 0)
+          {
+              ySpeed = Mathf.Lerp(ySpeed, 0.0f, 1.0f);
+          }*/
+        ySpeed = Mathf.Lerp(ySpeed, 0.0f, 1.0f);
+    }
+
+    void HorizontalRotation()
+    {
+        switch (goHorizontal)
+        {
+            case GoHorizontal.goRight:
+                GoRight();
+                return;
+            case GoHorizontal.goLeft:
+                GoLeft();
+                return;
+            case GoHorizontal.noDir:
+                StopHorizontalRotation();
+                return;
+        }
+    }
+    void GoRight()
+    {
+        //회전 방향에 맞게 가속도 줌
+        xSpeed += 5 * Time.deltaTime;
+
+
+        SpeedLimit(ref xSpeed);
+    }
+    void GoLeft()
+    {
+        //회전방향에 맞게 가속도 줌
+        xSpeed -= 5 * Time.deltaTime;
+
+        SpeedLimit(ref xSpeed);
+    }
+    void StopHorizontalRotation()
+    {
+        //마우스가 화면 중앙으로 오게되면 회전가속을 0으로 만들어
+        //더 이상 회전하지 않게 만듬
+        /*  if(ySpeed < 0)
+          {
+              //ySpeed += 10 * Time.deltaTime;
+              ySpeed = Mathf.Lerp(ySpeed, 0.0f, 1.0f);
+          }
+          else if(ySpeed > 0)
+          {
+              ySpeed = Mathf.Lerp(ySpeed, 0.0f, 1.0f);
+          }*/
+        xSpeed = Mathf.Lerp(xSpeed, 0.0f, 1.0f);
+    }
+
+
+    //회전 가속도 최대속도 제한
+    void SpeedLimit(ref float speed)
+    {
+        if(speed > maxTurningSpeed)
+        {
+            speed = maxTurningSpeed;
+        }
+        else if(speed < -maxTurningSpeed)
+        {
+            speed = -maxTurningSpeed;
+        }
+    }
+
 }
