aba2415bb8cbcca88e91a97b515b4299cb19189a
NongNoooo <83653104+NongNoooo@users.noreply.github.com>
2022-02-15 16:53:47 +0900

    타이틀화면

---
+++ Shooting/Assets/TimeLineEnd.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeLineEnd : MonoBehaviour
+{
+
+
+    private void OnEnable()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
